e14662d516f17c6ceb6b75e6bb25080ab6b47d22
chiudini <sbsy1517@gmail.com>
2020-01-31 15:52:02 +0800

    整理

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,11 @@ var jade = require('gulp-jade');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 
-gulp.task('pug', function () {           
-    return gulp.src('./source/**/*.pug')  
-        .pipe(pug({
-            pretty: true              
-        }))
-        .pipe(gulp.dest('./public/'))  
-});
- 
 gulp.task('sass', function () {
   return gulp.src('./source/sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
- 
 gulp.task('watch', function () {
     return gulp.watch('./srouce/sass/**/*.sass', gulp.series('sass'));
 });
@@ -34,3 +25,11 @@ gulp.task('jade', function() {
 });
 
 // gulp.task('default',['jade','sass','watch']);
+
+gulp.task('pug', function () {           
+  return gulp.src('./source/**/*.pug')  
+      .pipe(pug({
+          pretty: true              
+      }))
+      .pipe(gulp.dest('./public/'))  
+});
